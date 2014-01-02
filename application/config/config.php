13b2eabd78251148f8c2eda40021256f3fe2b124
Andrew Schorr <afs35mm@gmail.com>
2014-01-01 23:47:57 -0500

    modified config files for prod

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://date-shruti.dev';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
